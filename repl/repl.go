ac9dfc2acec5219cb05ed677dec389720bc8bceb
muyao.xiao <muyao.xiao@dena.com>
2022-07-01 15:39:20 +0800

    fix

--- repl/repl.go
+++ repl/repl.go
@@ -14,7 +14,7 @@ func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
 
 	for {
-		fmt.Fprintf(out, PROPMT)
+		fmt.Fprint(out, PROPMT)
 		scanned := scanner.Scan()
 		if !scanned {
 			return
