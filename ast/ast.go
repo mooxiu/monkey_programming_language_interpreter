4aed87b05df1d7617ba5499a70a29eb9028e3fac
mooxiu <monetshaw@outlook.com>
2022-07-02 23:10:26 +0800

    parse number ident

--- ast/ast.go
+++ ast/ast.go
@@ -78,6 +78,21 @@ func (i *Identifier) String() string {
 	return i.Value
 }
 
+type IntegerLiteral struct {
+	Token token.Token
+	Value int64
+}
+
+func (il IntegerLiteral) TokenLiteral() string {
+	return il.Token.Literal
+}
+
+func (il IntegerLiteral) String() string {
+	return il.Token.Literal
+}
+
+func (il IntegerLiteral) expressionNode() {}
+
 type ReturnStatement struct {
 	Token       token.Token // RETURN
 	ReturnValue Expression
