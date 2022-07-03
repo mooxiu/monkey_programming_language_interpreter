218f0122df66e133c2bd5267bbba6b23d62aaedd
mooxiu <monetshaw@outlook.com>
2022-07-03 18:54:22 +0800

    parse prefix

--- ast/ast.go
+++ ast/ast.go
@@ -126,3 +126,26 @@ func (es *ExpressionStatement) String() string {
 	}
 	return ""
 }
+
+type PrefixExpression struct {
+	Token    token.Token
+	Operator string
+	Right    Expression
+}
+
+func (pe *PrefixExpression) TokenLiteral() string {
+	return pe.Token.Literal
+}
+
+func (pe *PrefixExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("(")
+	out.WriteString(pe.Operator)
+	out.WriteString(pe.Right.String())
+	out.WriteString(")")
+
+	return out.String()
+}
+
+func (pe *PrefixExpression) expressionNode() {}
