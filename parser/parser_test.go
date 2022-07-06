803a37183dade256a7293d06537cfc4a462d03eb
muyao.xiao <muyao.xiao@dena.com>
2022-07-06 16:24:43 +0800

    add test for operator priorities

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -318,3 +318,67 @@ func TestParsingInfixExpressions(t *testing.T) {
 		}
 	}
 }
+
+func TestOperatorPrecedenceParsing(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{
+			input:    "-a * b",
+			expected: "((-a) * b)",
+		},
+		{
+			input:    "!-a",
+			expected: "(!(-a))",
+		},
+		{
+			input:    "a + b + c",
+			expected: "((a + b) + c)",
+		},
+		{
+			input:    "a + b - c",
+			expected: "((a + b) - c)",
+		},
+		{
+			input:    "a * b * c",
+			expected: "((a * b) * c)",
+		},
+		{
+			input:    "a * b / c",
+			expected: "((a * b) / c)",
+		},
+		{
+			input:    "a + b * c + d / e - f",
+			expected: "(((a + (b * c)) + (d / e)) - f)",
+		},
+		{
+			input:    "3 + 4; -5 * 5",
+			expected: "(3 + 4)((-5) * 5)",
+		},
+		{
+			input:    "5 > 4 == 3 < 4",
+			expected: "((5 > 4) == (3 < 4))",
+		},
+		{
+			input:    "5 < 4 != 3 > 4",
+			expected: "((5 < 4) != (3 > 4))",
+		},
+		{
+			input:    "3 + 4 * 5 == 3 * 1 + 4 * 5",
+			expected: "((3 + (4 * 5)) == ((3 * 1) + (4 * 5)))",
+		},
+	}
+
+	for _, tt := range tests {
+		l := lexer.New(tt.input)
+		p := New(l)
+		program := p.ParseProgram()
+		checkParserErrors(t, p)
+
+		actual := program.String()
+		if actual != tt.expected {
+			t.Errorf("expected=%q, got=%q", tt.expected, actual)
+		}
+	}
+}
